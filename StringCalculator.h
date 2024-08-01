b00e360c8d051f31970e2078b895f73ffa2bf240
spoortishetty09 <spoortishetty45@gmail.com>
2024-08-01 10:22:43 +0530

    Update StringCalculator.h

--- StringCalculator.h
+++ StringCalculator.h
@@ -3,6 +3,14 @@
 
 // Function prototype
 int string_calculator(const char *numbers);
+int add(const char* numbers);
+
+#ifndef STRING_CALCULATOR_H
+#define STRING_CALCULATOR_H
+
+
+
+#endif // STRING_CALCULATOR_H
 
 void test_empty_string() {
     assert(string_calculator("") == 0);
