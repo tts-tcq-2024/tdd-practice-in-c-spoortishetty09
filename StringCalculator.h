22d7f5dea5dd5198272bf1d695ab88948b211670
spoortishetty09 <spoortishetty45@gmail.com>
2024-08-01 11:32:02 +0530

    Update StringCalculator.h

--- StringCalculator.h
+++ StringCalculator.h
@@ -6,6 +6,12 @@
 // Function prototype
 
 int add(const char* numbers);
+void test_ignore_numbers_greater_than_1000();
+void test_custom_delimiter();
+void test_newline_delimiter();
+void test_two_numbers();
+void test_single_zero();
+void test_empty_input();
 
 
 
