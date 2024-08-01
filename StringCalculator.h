ac5402d4aeff1d220a62c004f17d8263ad31e1ad
spoortishetty09 <spoortishetty45@gmail.com>
2024-08-01 10:43:55 +0530

    Update StringCalculator.h

--- StringCalculator.h
+++ StringCalculator.h
@@ -1,12 +1,13 @@
 #include <stdio.h>
 #include <assert.h>
 
+#define STRING_CALCULATOR_H
+
 // Function prototype
 int string_calculator(const char *numbers);
 int add(const char* numbers);
 
-#ifndef STRING_CALCULATOR_H
-#define STRING_CALCULATOR_H
+
 
 
 
